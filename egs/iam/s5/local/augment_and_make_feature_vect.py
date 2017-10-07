97826bd5ef5dbba5f93e6806884b6ceba2f83fb1
YiwenShaoStephen <sywcs007wow@gmail.com>
2017-10-07 00:27:19 -0400

    fix bugs for parallel jobs

--- egs/iam/s5/local/augment_and_make_feature_vect.py
+++ egs/iam/s5/local/augment_and_make_feature_vect.py
@@ -15,7 +15,7 @@ parser = argparse.ArgumentParser(
     description="""Generates and saves the feature vectors""")
 parser.add_argument(
     'dir', type=str, help='directory of images.scp and is also output directory')
-parser.add_argument('--job', type=str, default='-1',
+parser.add_argument('--job', type=int, default='-1',
                     help='JOB number of images.JOB.scp if run in parallel mode')
 parser.add_argument('--out-ark', type=str, default='-',
                     help='where to write the output feature file')
@@ -255,7 +255,7 @@ with open(image_file) as image_fh:
 if args.job == -1:  # TODO
     scp_name = 'images.scp'  # non parallel
 else:
-    scp_name = 'images' + str(job) + 'scp'  # parallel
+    scp_name = 'images.' + str(args.job) + '.scp'  # parallel
 
 data_list_path = os.path.join(args.dir, 'backup', scp_name)
 
