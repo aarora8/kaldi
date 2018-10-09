f9250e8f202f341bd506b54c272d92e2c2302950
aarora8 <aarora8@jhu.edu>
2018-10-09 17:24:32 -0400

    adding augmentation

--- egs/rimes/v1/local/process_data.py
+++ egs/rimes/v1/local/process_data.py
@@ -21,7 +21,36 @@ parser.add_argument('dataset', type=str,
                     help='Subset of data to process.')
 args = parser.parse_args()
 
-def get_line_images_from_page_image(file_name, left, right, top, bottom, line_id, text):
+def dilate_polygon(points, amount_increase):
+    """ Increases size of polygon given as a list of tuples.
+        Assumes points in polygon are given in CCW
+    """
+    expanded_points = []
+    for index, point in enumerate(points):
+        prev_point = points[(index - 1) % len(points)]
+        next_point = points[(index + 1) % len(points)]
+        prev_edge = np.subtract(point, prev_point)
+        next_edge = np.subtract(next_point, point)
+
+        prev_normal = ((1 * prev_edge[1]), (-1 * prev_edge[0]))
+        prev_normal = np.divide(prev_normal, np.linalg.norm(prev_normal))
+        next_normal = ((1 * next_edge[1]), (-1 * next_edge[0]))
+        next_normal = np.divide(next_normal, np.linalg.norm(next_normal))
+
+        bisect = np.add(prev_normal, next_normal)
+        bisect = np.divide(bisect, np.linalg.norm(bisect))
+
+        cos_theta = np.dot(next_normal, bisect)
+        hyp = amount_increase / cos_theta
+
+        new_point = np.around(point + hyp * bisect)
+        new_point = new_point.astype(int)
+        new_point = new_point.tolist()
+        new_point = tuple(new_point)
+        expanded_points.append(new_point)
+    return expanded_points
+
+def get_line_images_from_page_image(file_name, left, right, top, bottom, line_id):
     image_path = os.path.join(data_path, file_name)
     im = Image.open(image_path)
     box = (left, top, right, bottom)
@@ -31,6 +60,9 @@ def get_line_images_from_page_image(file_name, left, right, top, bottom, line_id
     imgray = region.convert('L')
     image_path = os.path.join(data_path, 'lines', line_image_file_name)
     imgray.save(image_path)
+    return base_name, image_path
+
+def write_kaldi_process_data_files(image_path, base_name, line_id, text):
     writer_id = str(base_name.split('-')[1])
     writer_id = str(writer_id).zfill(6)
     writer_id = 'writer' + writer_id
@@ -64,4 +96,5 @@ for page in single_page:
         text = node.getAttribute('Value')
         text_vect = text.split() # this is to avoid non-utf-8 spaces
         text = " ".join(text_vect)
-        get_line_images_from_page_image(file_name, left, right, top, bottom, line_id, text)
+        base_name, image_path = get_line_images_from_page_image(file_name, left, right, top, bottom, line_id)
+        write_kaldi_process_data_files(image_path, base_name, line_id, text)
