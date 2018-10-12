6d397485c7d8176970f414e0c57034142e908df4
aarora8 <aarora8@jhu.edu>
2018-10-11 22:54:24 -0400

    adding scaling, shearning, contrast notmalization

--- egs/rimes/v1/local/extract_features.sh
+++ egs/rimes/v1/local/extract_features.sh
@@ -9,7 +9,7 @@
 nj=4
 cmd=run.pl
 feat_dim=40
-augment_type='no_aug'
+augment_type=true
 fliplr=false
 echo "$0 $@"
 
@@ -36,9 +36,9 @@ done
 utils/split_scp.pl $scp $split_scps || exit 1;
 
 $cmd JOB=1:$nj $logdir/extract_features.JOB.log \
-  image/ocr/make_features.py $logdir/images.JOB.scp \
+  local/make_features.py $logdir/images.JOB.scp \
     --allowed_len_file_path $data/allowed_lengths.txt \
-    --feat-dim $feat_dim --fliplr $fliplr --augment_type $augment_type \| \
+    --feat-dim $feat_dim --fliplr $fliplr --augment $augment_type \| \
     copy-feats --compress=true --compression-method=7 \
     ark:- ark,scp:$featdir/images.JOB.ark,$featdir/images.JOB.scp
 
