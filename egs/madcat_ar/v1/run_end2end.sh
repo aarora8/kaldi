7759135738a8a86987d19c3e5e8317e1019bb08a
aarora8 <aarora8@jhu.edu>
2019-02-20 16:48:32 -0500

    minor change

--- egs/madcat_ar/v1/run_end2end.sh
+++ egs/madcat_ar/v1/run_end2end.sh
@@ -19,7 +19,7 @@ images_scp_dir=data/local
 overwrite=false
 subset=false
 augment=false
-use_extra_corpus_text=false
+use_extra_corpus_text=true
 . ./cmd.sh ## You'll want to change cmd.sh to something that will work on your system.
            ## This relates to the queue.
 . ./path.sh
