34785b2c990d3733abeae03e54e4765b9a9baba2
aarora8 <aarora8@jhu.edu>
2021-03-01 23:56:17 -0500

    adding modifications to run gss with rttm

--- egs/chime6/s5e_track1/local/decode.sh
+++ egs/chime6/s5e_track1/local/decode.sh
@@ -45,137 +45,30 @@ enhanced_dir=$(utils/make_absolute.sh $enhanced_dir) || exit 1
 test_sets="dev_${enhancement} eval_${enhancement}"
 ./local/check_tools.sh || exit 1
 
-if [ $stage -le 0 ]; then
-  echo "$0:  prepare data..."
-  # dev worn is needed for the LM part
-  for dataset in dev eval; do
-    for mictype in u01 u02 u04 u06; do
-      local/prepare_data.sh --mictype ${mictype} \
-          ${audio_dir}/${dataset} ${json_dir}/${dataset} \
-          data/${dataset}_${mictype}
-      utils/validate_data_dir.sh --no-feats data/${dataset}_${mictype}
-    done
-  done
-
-  for dataset in dev eval; do
-    for mictype in u01 u02 u04 u06; do
-      utils/copy_data_dir.sh data/${dataset}_${mictype} data/${dataset}_${mictype}_ch1
-      utils/copy_data_dir.sh data/${dataset}_${mictype} data/${dataset}_${mictype}_ch2
-      utils/copy_data_dir.sh data/${dataset}_${mictype} data/${dataset}_${mictype}_ch3
-      utils/copy_data_dir.sh data/${dataset}_${mictype} data/${dataset}_${mictype}_ch4
- 
-      cp data/${dataset}_${mictype}_ch1/utt2spk data/${dataset}_${mictype}_ch1/utt2spk_temp
-      cp data/${dataset}_${mictype}_ch2/utt2spk data/${dataset}_${mictype}_ch2/utt2spk_temp
-      cp data/${dataset}_${mictype}_ch3/utt2spk data/${dataset}_${mictype}_ch3/utt2spk_temp
-      cp data/${dataset}_${mictype}_ch4/utt2spk data/${dataset}_${mictype}_ch4/utt2spk_temp
-
-      grep '.CH1-' data/${dataset}_${mictype}_ch1/utt2spk_temp > data/${dataset}_${mictype}_ch1/utt2spk
-      grep '.CH2-' data/${dataset}_${mictype}_ch2/utt2spk_temp > data/${dataset}_${mictype}_ch2/utt2spk
-      grep '.CH3-' data/${dataset}_${mictype}_ch3/utt2spk_temp > data/${dataset}_${mictype}_ch3/utt2spk
-      grep '.CH4-' data/${dataset}_${mictype}_ch4/utt2spk_temp > data/${dataset}_${mictype}_ch4/utt2spk
-
-      utils/fix_data_dir.sh data/${dataset}_${mictype}_ch1
-      utils/fix_data_dir.sh data/${dataset}_${mictype}_ch2
-      utils/fix_data_dir.sh data/${dataset}_${mictype}_ch3
-      utils/fix_data_dir.sh data/${dataset}_${mictype}_ch4
-
-      rm -r data/${dataset}_${mictype}
-    done
-  done
-fi
-
 if [ $stage -le 1 ]; then
   echo "$0:  enhance data..."
-#  if [ ! -d pb_chime5/ ]; then
-#    local/install_pb_chime5.sh
-#  fi
-#
-#  if [ ! -f pb_chime5/cache/chime6.json ]; then
-#    (
-#    cd pb_chime5
-#    miniconda_dir=$HOME/miniconda3/
-#    export PATH=$miniconda_dir/bin:$PATH
-#    export CHIME6_DIR=$chime6_corpus
-#    make cache/chime6.json
-#    )
-#  fi
-#
+  if [ ! -d pb_chime5/ ]; then
+    local/install_pb_chime5.sh
+  fi
 
-#  for dset in dev eval; do
-#    for reference_array in U01 U02 U04 U06; do
-#      local/run_gss.sh \
-#        --cmd "$train_cmd" --nj 100 \
-#        --multiarray False \
-#         --reference_array $reference_array \
-#        ${dset} \
-#        ${enhanced_dir}_$reference_array \
-#        ${enhanced_dir}_$reference_array || exit 1
-#    done
-#  done
+  if [ ! -f pb_chime5/cache/chime6.json ]; then
+    (
+    cd pb_chime5
+    miniconda_dir=$HOME/miniconda3/
+    export PATH=$miniconda_dir/bin:$PATH
+    export CHIME6_DIR=$chime6_corpus
+    make cache/chime6.json
+    )
+  fi
 
-  enhanced_dir=/export/c12/aarora8/CHiME_gss/s5_track1/enhanced_multiarray
   for dset in dev eval; do
-    for reference_array in U01 U02 U04 U06; do
-      local/prepare_data.sh --mictype gss --arrayid $reference_array \
-        ${enhanced_dir}_$reference_array/audio/${dset} ${json_dir}/${dset} \
-        data/${dset}_gss_$reference_array
-      utils/fix_data_dir.sh data/${dset}_gss_$reference_array
-      utils/validate_data_dir.sh --no-feats data/${dset}_gss_$reference_array || exit 1
-    done
+      local/run_gss_rttm.sh \
+        --cmd "$train_cmd" --nj 100 \
+        --multiarray outer_array_mics \
+        ${dset} ${enhanced_dir}_outer_array_mics \
+        ${enhanced_dir}_outer_array_mics || exit 1
   done
-fi
 
-if [ $stage -le 2 ]; then
-  echo "$0:  enhance data..."
-#  for dset in dev eval; do
-#      local/run_gss.sh \
-#        --cmd "$train_cmd" --nj 100 \
-#        --multiarray False \
-#        ${dset} ${enhanced_dir}_reference \
-#        ${enhanced_dir}_reference || exit 1
-#    done
-
-  enhanced_dir=/export/c12/aarora8/CHiME_gss/s5d_track1/enhanced_multiarray
-  for dset in dev eval; do
-      local/prepare_data.sh --mictype gss --arrayid reference \
-        ${enhanced_dir}_reference/audio/${dset} ${json_dir}/${dset} \
-        data/${dset}_gss_reference
-      utils/fix_data_dir.sh data/${dset}_gss_reference
-      utils/validate_data_dir.sh --no-feats data/${dset}_gss_reference
-  done
-fi
-
-if [ $stage -le 3 ]; then
-  echo "$0:  enhance data..."
-#  for dset in dev eval; do
-#      local/run_gss.sh \
-#        --cmd "$train_cmd" --nj 100 \
-#        --multiarray first_array_mics \
-#        ${dset} ${enhanced_dir}_first_array_mics \
-#        ${enhanced_dir}_first_array_mics || exit 1
-#    done
-
-  enhanced_dir=/export/c12/aarora8/CHiME_gss/s5d_track1/enhanced_multiarray
-  for dset in dev eval; do
-      local/prepare_data.sh --mictype gss --arrayid first_array_mics \
-        ${enhanced_dir}_first_array_mics/audio/${dset} ${json_dir}/${dset} \
-        data/${dset}_gss_first_array_mics
-      utils/fix_data_dir.sh data/${dset}_gss_first_array_mics
-      utils/validate_data_dir.sh --no-feats data/${dset}_gss_first_array_mics
-  done
-fi
-
-if [ $stage -le 4 ]; then
-  echo "$0:  enhance data..."
-#  for dset in dev eval; do
-#      local/run_gss.sh \
-#        --cmd "$train_cmd" --nj 100 \
-#        --multiarray outer_array_mics \
-#        ${dset} ${enhanced_dir}_outer_array_mics \
-#        ${enhanced_dir}_outer_array_mics || exit 1
-#  done
-
-  enhanced_dir=/export/c12/aarora8/CHiME_gss/enhanced_multiarray
   for dset in dev eval; do
       local/prepare_data.sh --mictype gss --arrayid outer_array_mics \
         ${enhanced_dir}_outer_array_mics/audio/${dset} ${json_dir}/${dset} \
@@ -185,30 +78,10 @@ if [ $stage -le 4 ]; then
   done
 fi
 
-if [ $stage -le 5 ]; then
-  echo "$0:  enhance data..."
-#  for dset in dev eval; do
-#      local/run_gss.sh \
-#        --cmd "$train_cmd" --nj 100 \
-#        --multiarray True \
-#        ${dset} ${enhanced_dir}_True \
-#        ${enhanced_dir}_True || exit 1
-#  done
 
-  enhanced_dir=enhanced_multiarray
-  #enhanced_dir=/export/c12/aarora8/CHiME_gss/s5_track1/enhanced_multiarray
+if [ $stage -le 2 ]; then
   for dset in dev eval; do
-      local/prepare_data.sh --mictype gss --arrayid True \
-        ${enhanced_dir}_True/audio/${dset} ${json_dir}/${dset} \
-        data/${dset}_gss_True
-      utils/fix_data_dir.sh data/${dset}_gss_True
-      utils/validate_data_dir.sh --no-feats data/${dset}_gss_True
-  done
-fi
-
-if [ $stage -le 6 ]; then
-  for dset in dev eval; do
-    for suffix in gss_U01 gss_U02 gss_U04 gss_U06 gss_reference gss_first_array_mics gss_outer_array_mics gss_True; do
+    for suffix in gss_outer_array_mics; do
       utils/copy_data_dir.sh data/${dset}_${suffix} data/${dset}_${suffix}_orig
       utils/data/modify_speaker_info.sh --seconds-per-spk-max 180 data/${dset}_${suffix}_orig data/${dset}_${suffix}
       if [ ! -s data/${dset}_${suffix}_hires/feats.scp ]; then
@@ -221,31 +94,9 @@ if [ $stage -le 6 ]; then
   done
 fi
 
-chime6_corpus=${PWD}/CHiME6_new
-json_dir=${chime6_corpus}/transcriptions
-audio_dir=${chime6_corpus}/audio
-if [ $stage -le 7 ]; then
-  echo "$0:  prepare data..."
-  for dataset in dev eval; do
-    for mictype in worn; do
-      local/prepare_data.sh --mictype ${mictype} \
-          ${audio_dir}/${dataset} ${json_dir}/${dataset} \
-          data/${dataset}_${mictype}
-      utils/copy_data_dir.sh data/${dataset}_${mictype} data/${dataset}_${mictype}_stereo
-      grep "\.L-" data/${dataset}_${mictype}_stereo/text > data/${dataset}_${mictype}/text
-      utils/fix_data_dir.sh data/${dataset}_${mictype}
-      utils/copy_data_dir.sh data/${dataset}_${mictype} data/${dataset}_${mictype}_orig
-      utils/data/modify_speaker_info.sh --seconds-per-spk-max 180 data/${dataset}_${mictype}_orig data/${dataset}_${mictype}
-      utils/copy_data_dir.sh data/${dataset}_${mictype} data/${dataset}_${mictype}_hires
-      steps/make_mfcc.sh --mfcc-config conf/mfcc_hires.conf --nj 80 --cmd "$train_cmd" data/${dataset}_${mictype}_hires
-      steps/compute_cmvn_stats.sh data/${dataset}_${mictype}_hires
-      utils/fix_data_dir.sh data/${dataset}_${mictype}_hires
-    done
-  done
-fi
 
-if [ $stage -le 8 ]; then
-  for suffix in worn gss_U01 gss_U02 gss_U04 gss_U06 gss_reference gss_first_array_mics gss_outer_array_mics gss_True; do
+if [ $stage -le 3 ]; then
+  for suffix in gss_outer_array_mics; do
     local/chain/run_cnn_tdnn.sh --stage 16 --enhancement $suffix
   done
 fi
