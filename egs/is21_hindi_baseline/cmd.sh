79daa8959dd2e75dd292302b2b3ecd1f5eb59c68
System User <aarora@test2.cm.gemini>
2021-03-10 11:27:26 -0500

    setting up coe

--- egs/is21_hindi_baseline/cmd.sh
+++ egs/is21_hindi_baseline/cmd.sh
@@ -10,6 +10,17 @@
 # conf/queue.conf in http://kaldi-asr.org/doc/queue.html for more information,
 # or search for the string 'default_config' in utils/queue.pl or utils/slurm.pl.
 
-export cmd="queue.pl --mem 4G"
-export train_cmd="queue.pl --mem 4G"
-export decode_cmd="queue.pl --mem 4G"
+export train_cmd="queue.pl --mem 8G --nodes_rack 4"
+export decode_cmd="queue.pl --mem 8G --nodes_rack 4"
+export train_cmd_intel="queue.pl --mem 8G --nodes_rack 4"
+export train_cmd_all_intel="queue.pl --mem 8G --remove_nodes_rack 7"
+export train_cmd_r5="queue.pl --mem 16G --nodes_rack 5 --gpu_queue 1"
+export train_cmd_r7="queue.pl --mem 16G --nodes_rack 7 --gpu_queue 1"
+export train_cmd_r6="queue.pl --mem 16G --nodes_rack 6 --gpu_queue 1"
+export train_cmd_r8="queue.pl --mem 16G --nodes_rack 8 --gpu_queue 1"
+export train_cmd_tesla="queue.pl --mem 16G --nodes_tesla 1 --gpu_queue 1"
+export gpu_cmd="queue.pl --mem 8G --gpu 1"
+
+
+
+
